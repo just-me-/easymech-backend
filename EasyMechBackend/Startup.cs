ec8f9e7ba7cff9af6d786c06ff9b648b818ec4cb
Tom852 <thomaskistler@bluewin.ch>
2019-04-08 17:54:38 +0200

    Hotfix CORS

--- EasyMechBackend/Startup.cs
+++ EasyMechBackend/Startup.cs
@@ -45,6 +45,8 @@ namespace EasyMechBackend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("AllowPolicy");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
